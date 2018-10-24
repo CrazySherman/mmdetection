0d81808681311a08cff7b2a805a3fbf2fd28e2f3
Sherman <wsmpku2015@gmail.com>
2018-10-24 00:03:49 -0700

    finished airbus dataset

--- configs/airbus_mask_rcnn_r50_fpn_1x.py
+++ configs/airbus_mask_rcnn_r50_fpn_1x.py
@@ -24,12 +24,12 @@ model = dict(
         target_means=[.0, .0, .0, .0],
         target_stds=[1.0, 1.0, 1.0, 1.0],
         use_sigmoid_cls=True),
-    bbox_roi_extractor=dict(
-        type='SingleRoIExtractor',
-        roi_layer=dict(type='RoIAlign', out_size=7, sample_num=2),
-        out_channels=256,
-        featmap_strides=[4, 8, 16, 32]),
-    # may be helpful to co-train with mask, RoI-bbox regressor/cls is a better learnable 
+    # may be helpful to co-train with mask, RoI-bbox regressor/cls is a better learnable? 
+    # bbox_roi_extractor=dict(
+    #     type='SingleRoIExtractor',
+    #     roi_layer=dict(type='RoIAlign', out_size=7, sample_num=2),
+    #     out_channels=256,
+    #     featmap_strides=[4, 8, 16, 32]),
     # bbox_head=dict(           
     #     type='SharedFCRoIHead',
     #     num_fcs=2,
@@ -93,51 +93,32 @@ test_cfg = dict(
         score_thr=0.05, max_per_img=100, nms_thr=0.5, mask_thr_binary=0.5))
 # dataset settings
 dataset_type = 'AirBusKaggle'
-data_root = 'data/airbus/'
+data_root = '/data/airbus/'
 
-#TODO calculate this shit from dataset
+# this is imagenet pretrained params
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
-#TODO build Dataset class from this shit
 data = dict(
     imgs_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_train2017.json',
-        img_prefix=data_root + 'train2017/',
-        img_scale=(1333, 800),
+        #TODO figure this out, MaskCNN uplift the resolution for a reason
+        img_scale=(1333, 800),    
+        data_root=data_root,  
         img_norm_cfg=img_norm_cfg,
         size_divisor=32,
-        flip_ratio=0.5,
-        with_mask=True,
-        with_crowd=True,
-        with_label=True),
+        flip_ratio=0.5),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
         img_scale=(1333, 800),
         img_norm_cfg=img_norm_cfg,
         size_divisor=32,
-        flip_ratio=0,
-        with_mask=True,
-        with_crowd=True,
-        with_label=True),
-    test=dict(
-        type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
-        img_scale=(1333, 800),
-        img_norm_cfg=img_norm_cfg,
-        size_divisor=32,
-        flip_ratio=0,
-        with_mask=False,
-        with_label=False,
-        test_mode=True))
+        flip_ratio=0)
+   )
 # optimizer
-#TODO figure out learning rate...one GPU should be 0.002
+#figure out learning rate...one GPU should be 0.002
 optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
@@ -157,7 +138,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-#TODO try out epoch nums
+#try out epoch nums
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
--- mmdet/datasets/airbus.py
+++ mmdet/datasets/airbus.py
@@ -4,28 +4,81 @@ import numpy as np
 from mmcv.parallel import DataContainer as DC
 from pycocotools.coco import COCO
 from torch.utils.data import Dataset
+import logging
 
 from .transforms import (ImageTransform, BboxTransform, MaskTransform,
                          Numpy2Tensor)
 from .utils import to_tensor, show_ann, random_scale
+import pandas as pd 
 
+# https://www.kaggle.com/inversion/run-length-decoding-quick-start
+# see visualization: https://www.kaggle.com/meaninglesslives/airbus-ship-detection-data-visualization
+def rle_decode(mask_rle, shape=(768, 768)):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return 
+    Returns numpy array, 1 - mask, 0 - background
 
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape).T  # Needed to align to RLE direction
+
+class MaskLoader:
+    def __init__(self, data_root):
+         # load rle file from csv
+        self.masks_rle = pd.read_csv(os.path.join(data_root, 'train_ship_segmentations_v2.csv'))
+        # drop the na
+        self.masks_rle.dropna(inplace=True)
+
+    def __getitem__(self, imageId):
+        return self._get_mask_from_rle(imageId)
+
+    # TODO peformance benchmarking this shit 
+    def _get_mask_from_rle(self, imageId):
+        mask = self.masks_rle.loc[masks['ImageId'] == imageId, 'EncodedPixels'].tolist()
+        if len(mask) == 0:
+            logging.warning('trying to get an empty ship mask')
+            return None
+        res = np.zeros((768, 768))
+        for m in mask:
+            res += rle_decode(m)
+        return res
+
+    def get_no_ship_image_ids():
+        null_masks = masks['EncodedPixels'].isnull()
+        selected = masks['ImageId'][null_masks].tolist()
+        empty_ships = set()
+        for s in selected:
+            empty_ships.add(s)
+        return empty_ships
 
 
 class AirbusKaggle(Dataset):
 	def __init__(self, img_scale, data_root, img_norm_cfg,
 				flip_ratio=0,
 				size_divisor=None,
-				test_mode=False,
-
+				test_mode=False
 				):
-		self.data_root = data_root
-		#TODO prepare masks from raw encodings or precomputed files?
-		self.img_ids = []
-		self.masks = dict()  
+	   
+        logging.info('starting scanning data root: ', data_root)
+        if test_mode:
+            self.img_root, self.img_ids = self.get_all_test_image_ids(data_root)
+        else:
+    		self.img_root, self.img_ids = self.get_all_train_image_ids(data_root)
+
+        # MaskLoader as the proxy for panda processing
+        self.masks =  MaskLoader(data_root)	
+        logging.info('Masks successfully loaded')
 
 		# filter images with no ships, this important otherwise get(idx) will return null to dataloader
 		if not test_mode:
+            logging.info('train mode, start filtering empty ship images...')
 			self._filter_images()
 		# color channel order and normalize configs
         self.img_norm_cfg = img_norm_cfg
@@ -40,8 +93,23 @@ class AirbusKaggle(Dataset):
 		# (long_edge, short_edge) or [(long1, short1), (long2, short2), ...]
         self.img_scales = img_scale if isinstance(img_scale,
                                                   list) else [img_scale]
-        # set group flag for the sampler 
-        self._set_group_flag()
+        # # set group flag for the sampler 
+        # self._set_group_flag()
+        
+        print('Airbus Dataset Summary \n'
+            '\ttest_mode: {} \n'
+            '\timg_norm_cfg: {} \n'
+            '\tsize_divisor: {} \n'
+            '\tflip_ratio: {} \n'
+            '\timage_scales: {} \n'
+            .format(
+                self.test_mode,
+                self.img_norm_cfg,
+                self.size_divisor,
+                self.flip_ratio,
+                self.img_scales
+                ))
+
 
 	#TODO add bbox? may or may not be helpful 
 	def __getitem__(self, idx):
@@ -55,7 +123,7 @@ class AirbusKaggle(Dataset):
 
 		image_id = self.img_ids[idx]
 		masks = self.masks[image_id]
-		img = mmcv.imread(os.path.join(self.data_root, image_id))
+		img = mmcv.imread(os.path.join(self.img_root, image_id))
 		ori_shape = img.shape
 		 # apply transforms
         flip = True if np.random.rand() < self.flip_ratio else False
@@ -86,24 +154,28 @@ class AirbusKaggle(Dataset):
 	def __len__(self):
         return len(self.img_ids)
 	
-	def _set_group_flag(self):
-        """Set flag according to image aspect ratio.
+	# def _set_group_flag(self):
+ #        """Set flag according to image aspect ratio.
 
-        Images with aspect ratio greater than 1 will be set as group 1,
-        otherwise group 0.
-        """
-        self.flag = np.zeros(len(self.img_ids), dtype=np.uint8)
-        for i in range(len(self.img_ids)):
-            img_info = self.img_infos[i]
-            if img_info['width'] / img_info['height'] > 1:
-                self.flag[i] = 1
+ #        Images with aspect ratio greater than 1 will be set as group 1,
+ #        otherwise group 0.
+ #        """
+ #        self.flag = np.zeros(len(self.img_ids), dtype=np.uint8)
+ #        for i in range(len(self.img_ids)):
+ #            img_info = self.img_infos[i]
+ #            if img_info['width'] / img_info['height'] > 1:
+ #                self.flag[i] = 1
 
-    #TODO implement this.
+    # during training, remove the images without gt
     def _filter_images(self):
-    	pass
+    	bad_image_ids = self.masks.get_no_ship_image_ids()
+        filtered_imgs = [i for i in self.img_ids if i in bad_image_ids]
+        logging.info('Image Count before filtering: ', len(self))        
+        self.img_ids = filtered_imgs
+        logging.info('Image Count after filtering: ', len(self))
 
     def prepare_test_img(self, idx):
-    	img = mmcv.imread(os.path.join(self.data_root, img_ids[idx]))
+    	img = mmcv.imread(os.path.join(self.img_root, img_ids[idx]))
     	ori_shape = img.shape
     	def prepare_single(img, scale, flip, proposal=None):
             """
@@ -148,5 +220,15 @@ class AirbusKaggle(Dataset):
 
         data = dict(img=imgs, img_meta=img_metas)
         return data
+
+    def get_all_train_image_ids(data_root):
+        imgs = os.listdir(os.path.join(data_root, 'train_v2'))
+        print('Train images are like: ', imgs[:10])
+        return os.path.join(data_root, 'train_v2'), imgs
+    def get_all_test_image_ids(data_root):
+        imgs = os.listdir(os.path.join(data_root, 'test_v2'))        
+        print('Test images are like: ', imgs[:10])
+        return os.path.join(data_root, 'test_v2'), imgs
+
 
 
